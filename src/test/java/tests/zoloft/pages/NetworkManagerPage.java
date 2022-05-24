7c12043a4f5f05b621af5b2a044b41d6d1a34456
Max Zhurkin <99205557+Zo1oft@users.noreply.github.com>
2022-05-24 22:41:57 +0500

    Update NetworkManagerPage.java

--- src/test/java/tests/zoloft/pages/NetworkManagerPage.java
+++ src/test/java/tests/zoloft/pages/NetworkManagerPage.java
@@ -6,12 +6,12 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$;
 
 public class NetworkManagerPage {
+    
     //locator
     SelenideElement
         pageName = $(".products_line__logo");
 
     //action
-
     public void networkPageCheck() {
         pageName.shouldHave(Condition.text("Network Manager"));
     }
