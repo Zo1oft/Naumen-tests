11a546f6a98b1cc8db0a7228d6f79180fb17b25f
Max Zhurkin <99205557+Zo1oft@users.noreply.github.com>
2022-05-24 22:42:23 +0500

    Update SearchPage.java

--- src/test/java/tests/zoloft/pages/SearchPage.java
+++ src/test/java/tests/zoloft/pages/SearchPage.java
@@ -6,12 +6,12 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SearchPage {
+    
     //locator
     SelenideElement
             searchResultList = $(".b-serp-list");
 
     //action
-
     public void searchListCheck() {
         searchResultList.shouldHave(Condition.text("контакт-центр"));
     }
